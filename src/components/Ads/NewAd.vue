975c2f5b7ac18fe150c2dfeacdc256441b695f13
Aminna07 <umerova.a.i.20@gmail.com>
2022-01-23 15:58:17 +0300

    Создание компонентов всех страниц

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,80 +1,17 @@
 <template> 
-    <v-container>
-        <v-layout row> 
-            <v-flex xs12 sm6 offset-sm3>
-                <h1 class="text--secondary mb-3 mt-3">Create Ad</h1>
-                <v-form v-model="valid" ref="form" validation>
-                  <v-text-field color="#041562"
-                     name="title" 
-                     label="Ad Title" 
-                     type="text" 
-                     v-model="title"
-                     :rules="[ v => !!v || 'Title is required']">
-                 </v-text-field>
-                  <v-textarea color="#041562"  
-                     name="description" 
-                     label="Ad Description" 
-                     type="text" 
-                     v-model="description"
-                     :rules="[ v => !!v || 'Description is required']"
-                     class="mb-3" >
-                 </v-textarea>
-                </v-form>
-
-                <v-layout  row> 
-                  <v-flex xs12>
-                      <v-btn class="mt-3" color="warning">
-                          Upload
-                         <v-icon right dark>mdi-cloud-upload</v-icon>
-                      </v-btn>
-
-                    </v-flex>
-                </v-layout>
-
-                <v-layout  row> 
-                 <v-flex xs12>
-                      <img src="https://chita.klumba.store/upload/iblock/1eb/ptaxrdx3w70dsndi3pnsx8xdv1r1jmkf.JPG" height="150" class="mt-3">
-                 </v-flex>
-                </v-layout>
-
-                <v-layout  row> 
-                  <v-flex xs12>
-                      <v-switch v-model="promo" color="#041562" label="Ad to Promo?"></v-switch>
-                    </v-flex>
-                </v-layout>
-
-                <v-layout  row> 
-                  <v-flex xs12>
-                      <v-btn color="success" @click="createAd">Create Ad</v-btn>
-                  </v-flex>
-                </v-layout>
-            </v-flex>
-        </v-layout> 
-    </v-container>
+	<v-container>
+		<v-layout row> 
+			<v-flex xs12>
+                <h1>NewAd</h1> 
+                </v-flex>
+		</v-layout> 
+	</v-container>
 </template>
-
 <script>
 export default {
-    data () { 
-        return {
-            valid: false,
-            title: "",
-            description: "",
-            promo: false
-        } 	
-    },
-    methods: {
-      createAd(){
-        if (this.$refs.form.validate()){
-            const ad = {
-                title: this.title,
-                desc: this.description,
-                promo: this.promo,
-                src: "https://chita.klumba.store/upload/iblock/1eb/ptaxrdx3w70dsndi3pnsx8xdv1r1jmkf.JPG"
-                }
-        this.$store.dispatch("createAd", ad)
-    }
-   }
-  }
+	data () { 
+		return {
+		} 	
+	}
 } 
 </script>
--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,78 +1,17 @@
 <template> 
 	<v-container>
 		<v-layout row> 
-		<v-flex xs12 sm6 offset-sm3>
-					<h1 class="text--secondary mb-3 mt-3">Orders</h1>
-	<v-card
-    class="mx-auto"
-    max-width="300"
-    tile
-  >
-    <v-list subheader
-    two-line
-    flat>
-        <v-list-item v-for="order in orders" :key="order.id">
-          
-          <v-list-item-content>
-            <v-list-item-title>{{order.name}}</v-list-item-title>
-			<v-list-item-subtitle>{{order.phone}}</v-list-item-subtitle>
-          </v-list-item-content>
-
-	<v-list-item-action>
-		<v-checkbox
-		:input-value="order.done"
-		
-		color="#7C99AC"
-    @click="markDone(order)">
-		</v-checkbox>
-    </v-list-item-action>
-
-	<v-list-item-action>
-		<v-btn class="#FFEFEF" :to="'/ad/' + order.adId">Open</v-btn>
-    </v-list-item-action>
-
-        </v-list-item>
-    </v-list>
-  </v-card>
-		</v-flex>
+			<v-flex xs12>
+                <h1>Orders</h1> 
+                </v-flex>
 		</v-layout> 
 	</v-container>
 </template>
-
 <script>
-  export default {
-    data() {
-      return{
-        orders:[
-          {
-            id:"111",
-            name:"Kate",
-            phone:"+79785630593",
-            adId:"1",
-            done:true
-          },
-          {
-            id:"112",
-            name:"Daniel",
-            phone:"+79786784524",
-            adId:"2",
-            done:true
-          },
-          {
-            id:"113",
-            name:"Max",
-            phone:"+79781230004",
-            adId:"3",
-            done:true
-          }
-        ]
-      }
-    },
-    mathods:{
-      markDone(order){
-        order.done=!order.done
-        console.log(order.done)
-      }
-    } 
-  }
+export default {
+	data () { 
+		return {
+		} 	
+	}
+} 
 </script>
